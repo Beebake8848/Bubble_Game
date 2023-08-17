54077ab133f818f7a74e55f1edb78e16f5f89e00
Beebake8848 <kbibek448@gmail.com>
2023-08-18 00:37:18 +0545

    adding

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+
+function increaseScore(){
+    score +=10;
+    document.querySelector("#scoreval").textContent=score
+}
+
+function runTimer(){
+    var timerint = setInterval(function(){
+        if(timer>0){
+        timer--;
+        document.querySelector("#timerval").textContent=timer;
+    }
+    else{
+        clearInterval(timerint);
+        document.querySelector("#pbtm").innerHTML=`<h1>Game Over</h1>`;
+    }
+    },1000);
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").innerHTML=hitrn;
+}
+
 function makebubble(){
     var clutter = "";
 
@@ -9,22 +37,18 @@ for(var i=1; i<=168; i++){
 document.querySelector("#pbtm").innerHTML=clutter
 }
 
-var timer = 60;
-function runTimer(){
-    var timerint = setInterval(function(){
-        if(timer>0){
-        timer--;
-        document.querySelector("#timerval").textContent=timer;
+document.querySelector("#pbtm").addEventListener("click",
+function(dets){
+    var clickednum = Number(dets.target.textContent);
+    if(clickednum === hitrn){
+        increaseScore();
+        makebubble();
+        getNewHit();
     }
-    else{
-        clearInterval(timerint);
-    }
-    },1000);
-}
+});
 
-function getNewHit(){
-    
-}
 
+increaseScore();
 runTimer();
 makebubble();
+getNewHit();
